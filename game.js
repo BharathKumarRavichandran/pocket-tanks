428c3dcb2488ce0c4cc73b0eed2d941858a250ec
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-05-20 23:35:39 +0530

    Pause, Resume, Quit, Gameover functionalities

--- game.js
+++ game.js
@@ -28,6 +28,8 @@ var player2 = "Player 2";
 var score1 = 0;
 var score2 = 0;
 var pause = false;
+var quit = false;
+var gameOver = false;
 
 var bg1 = new Image();
 var bg2 = new Image();
@@ -139,6 +141,22 @@ document.addEventListener('keydown', function(event){
 					}
 				}
         	}
+
+        	if(event.keyCode==80){//p pause/resume
+        		if(pause==false){
+        			pause=true;
+        		}
+        		else{
+        			pause=false;
+        			animation();
+        		}
+        	}
+        	if(event.keyCode==82){//r restart
+        		window.location.reload();
+        	}
+        	if(event.keyCode==81){//q quit
+        		quit=true;
+        	}
     },false);
 
 
@@ -229,13 +247,54 @@ function drawTank1(){
 }
 
 function drawTank2(){
-	//ctx.rotate(20*Math.PI/180);
 	ctx.drawImage(tank2,1100,370,tankWidth,tankHeight);
 	ctx.restore();
 }
 
-function animation(){
+function pauseGameDraw(){
+	ctx.fillStyle = "#000000";
+	ctx.globalAlpha = 0.6;
+	ctx.fillRect(canvasWidth-canvasWidth*0.73,canvasHeight-canvasHeight*0.8,600,300);
+	ctx.globalAlpha = 1;
+	ctx.fillStyle = "#FF0000";
+	ctx.font = "40px Trebuchet MS";
+	ctx.fillText("GAME PAUSED",canvasWidth-canvasWidth*0.59,canvasHeight-canvasHeight*0.65);
+	ctx.font = "30px Trebuchet MS";
+	ctx.fillStyle = "#FFFFFF";
+	ctx.fillText("Press P to resume",canvasWidth-canvasWidth*0.59,canvasHeight-canvasHeight*0.52);
+	ctx.fillText("Press R to restart",canvasWidth-canvasWidth*0.59,canvasHeight-canvasHeight*0.40);
+}
 
+function quitGameDraw(){
+	ctx.fillStyle = "#000000";
+	ctx.globalAlpha = 0.6;
+	ctx.fillRect(canvasWidth-canvasWidth*0.73,canvasHeight-canvasHeight*0.8,600,300);
+	ctx.globalAlpha = 1;
+	ctx.fillStyle = "#FF0000";
+	ctx.font = "40px Trebuchet MS";
+	ctx.fillText("Are you sure to Quit?",canvasWidth-canvasWidth*0.64,canvasHeight-canvasHeight*0.65);
+	ctx.font = "30px Trebuchet MS";
+	ctx.fillStyle = "#FFFFFF";
+	ctx.fillText("Press P to resume",canvasWidth-canvasWidth*0.59,canvasHeight-canvasHeight*0.52);
+	ctx.fillText("Press R to restart",canvasWidth-canvasWidth*0.59,canvasHeight-canvasHeight*0.40);
+}
+
+function gameOverDraw(){//end screen to draw on canvas when the game is over
+	ctx.fillStyle = "#000000";
+	ctx.globalAlpha = 0.6;
+	ctx.fillRect(canvasWidth-canvasWidth*0.73,canvasHeight-canvasHeight*0.8,600,300);
+	ctx.globalAlpha = 1;
+	ctx.fillStyle = "#FF0000";
+	ctx.font = "40px Trebuchet MS";
+	ctx.fillText("GAME OVER",canvasWidth-canvasWidth*0.59,canvasHeight-canvasHeight*0.65);
+	ctx.font = "30px Trebuchet MS";
+	ctx.fillStyle = "#FFFFFF";
+	ctx.fillText(player1+" : "+score1,canvasWidth-canvasWidth*0.68,canvasHeight-canvasHeight*0.53);
+	ctx.fillText(player2+" : "+score2,canvasWidth-canvasWidth*0.45,canvasHeight-canvasHeight*0.53);
+	ctx.fillText("Press R to restart",canvasWidth-canvasWidth*0.60,canvasHeight-canvasHeight*0.40);
+}
+
+function animation(){
 	drawAssets();
 	drawValues();
 	drawHill();
@@ -251,8 +310,28 @@ function animation(){
 	ctx.fillStyle = "darkred";
 	ctx.fillText("Player Active: "+playerActive,20,160);
 	ctx.fillText("Pause: P",1125,160);
-	ctx.fillText("Resume: R",1100,193);
+	ctx.fillText("Quit: Q",1138,193);
 	ctx.fillStyle = "#FFFFFF";
+
+
+	if(pause==true){
+		if(quit==true){
+			ctx.clearRect(canvasWidth-canvasWidth*0.73,canvasHeight-canvasHeight*0.8,600,300);
+			quit=false;
+		}
+		pauseGameDraw();
+		return;
+	}
+	if(quit==true){
+		quitGameDraw();
+		return;
+	}
+
+	if(moves1==0&&moves2==0){//Gameover function
+		gameOver==true;
+		gameOverDraw();
+		return;
+	}
 
 	requestAnimationFrame(animation);
 }
